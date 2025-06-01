f9d4e874fa99fca9ece612f0ef51f24f2683096e
Rejaul Karim <createwithkarim@Rejauls-MacBook-Air.local>
2025-06-01 23:24:17 +0600

    feat: added stats card

--- src/app/(root)/store/orders/page.tsx
+++ src/app/(root)/store/orders/page.tsx
@@ -1,5 +1,36 @@
-const OrdersPage = () => {
-  return <div>OrdersPage</div>;
+import StatsCard from "@/components/Shared/Cards/StatsCard";
+
+const OrderPage = () => {
+  return (
+    <section className="py-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <StatsCard
+          title="Total Revenue"
+          amount="$1,250.00"
+          value="+12.5%"
+          info="Visitors for the last 6 months"
+        />
+        <StatsCard
+          title="New Customers"
+          amount="1,234"
+          value="+12.5%"
+          info="Acquisition needs attention"
+        />
+        <StatsCard
+          title="Active Accounts"
+          amount="45,678"
+          value="+12.5%"
+          info="Engagement exceed targets"
+        />
+        <StatsCard
+          title="Growth Rate"
+          amount="4.5%"
+          value="+12.5%"
+          info="Meets growth projections"
+        />
+      </div>
+    </section>
+  );
 };
 
-export default OrdersPage;
+export default OrderPage;
---
+++ src/components/Shared/Cards/StatsCard.tsx
@@ -0,0 +1,35 @@
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { TrendingUpIcon } from "lucide-react";
+
+const StatsCard = ({ amount, value, label, info }: any) => {
+  return (
+    <Card className="@container/card">
+      <CardHeader className="relative">
+        <CardDescription>{label}</CardDescription>
+        <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
+          {amount}
+        </CardTitle>
+        <div className="absolute right-4 top-4">
+          <Badge variant="outline" className="flex gap-1 rounded-lg text-xs">
+            <TrendingUpIcon className="size-3" />
+            {value}
+          </Badge>
+        </div>
+      </CardHeader>
+      <CardFooter className="flex-col items-start gap-1 text-sm">
+        <div className="line-clamp-1 flex gap-2 font-medium">
+          {info} <TrendingUpIcon className="size-4" />
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default StatsCard;
